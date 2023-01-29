c9178b0a6d0e16c29b58e199a7acdc34bbdeeeec
Andrew-LeGrand <111323440+Andrew-LeGrand@users.noreply.github.com>
2023-01-29 12:39:34 -0600

    instance variable
    instance variables and view templates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
     def new
-        random_number = rand(1..10)
-        puts random_number
-        if random_number < 5
+        @random_number = rand(1..10)
+        puts @random_number
+        if @random_number < 5
             # if current user is logged in, redirect to about us
             # redirect to localhost:3000/about-us and call the about_us action from the static pages controller
             redirect_to '/about-us'
